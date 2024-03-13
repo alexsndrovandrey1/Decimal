0c5c9083e39e2bc116f62623e42fed1e6922cf6d
Andrey Alexsandrov <an.alex25@mail.ru>
2024-03-13 22:57:16 +0300

    other header

---
+++ src/headers/s21_other.h
@@ -0,0 +1,13 @@
+#ifndef SRC_S21_OTHER_H_
+#define SRC_S21_OTHER_H_
+
+
+//Округление
+int s21_floor(s21_decimal value, s21_decimal *result);
+int s21_negate(s21_decimal value, s21_decimal *result);
+int s21_round(s21_decimal value, s21_decimal *result);
+int s21_truncate(s21_decimal value, s21_decimal *result);
+s21_decimal *s21_copy(s21_decimal *dest, s21_decimal src);
+s21_decimal *s21_reset(s21_decimal *value);
+
+#endif
